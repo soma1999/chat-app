05fa875e95188b7caedf30a5e7b8c64fa502977a
soma1999 <so12ma131999@gmail.com>
2021-01-26 18:58:51 +0900

    メッセージ送信機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   get 'messages/index'
   root to: "rooms#index"
   resources :users, only: [:edit, :update]
-  resources :rooms, onlt: [:new, :create]
+  resources :rooms, onlt: [:new, :create] do
+    resources :messages, only: [:index, :create]
+  end
 end
